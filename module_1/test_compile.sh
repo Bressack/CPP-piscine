55576f7fbc43381ab9d054bfd4b6de1c81a39c5a
bressack <tharchen@student.42.fr>
2021-06-25 14:51:13 +0200

    auto commit from alias

--- module_1/test_compile.sh
+++ module_1/test_compile.sh
@@ -1,9 +1,14 @@
 
-echo "Compilation of ex00:" ; make re -C ex00
-echo "Compilation of ex01:" ; make re -C ex01
-echo "Compilation of ex02:" ; make re -C ex02
-echo "Compilation of ex03:" ; make re -C ex03
-echo "Compilation of ex04:" ; make re -C ex04
-echo "Compilation of ex05:" ; make re -C ex05
-echo "Compilation of ex06:" ; make re -C ex06
-echo "Compilation of ex07:" ; make re -C ex07
+makefile_rules()
+{
+	echo "Compilation of ex00:" ; make $1 -C ex00
+	# echo "Compilation of ex01:" ; make $1 -C ex01
+	echo "Compilation of ex02:" ; make $1 -C ex02
+	echo "Compilation of ex03:" ; make $1 -C ex03
+	echo "Compilation of ex04:" ; make $1 -C ex04
+	echo "Compilation of ex05:" ; make $1 -C ex05
+	echo "Compilation of ex06:" ; make $1 -C ex06
+	echo "Compilation of ex07:" ; make $1 -C ex07
+}
+
+makefile_rules $1
