6b1478cc1f24ee1808918169242df414e041c787
bressack <tharchen@student.42.fr>
2021-07-13 12:13:45 +0200

    auto commit from alias

---
+++ module_8/ex00/includes/easyfind.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tharchen <tharchen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/12 22:44:01 by tharchen          #+#    #+#             */
+/*   Updated: 2021/07/13 11:42:49 by tharchen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <algorithm>
+
+class NoMatchException: public std::exception
+{
+	public:
+		NoMatchException(void) {}
+		const char *what() const throw()
+		{
+			return ("no match found");
+		}
+};
+
+template<typename T>
+typename T::iterator easyfind(T & cont, int value)
+{
+	typename T::iterator ret;
+
+	ret = std::find(cont.begin(), cont.end(), value);
+	if (ret == cont.end())
+		throw NoMatchException();
+	return (ret);
+}
+
+#endif
---
+++ module_8/ex00/srcs/main.cpp
@@ -0,0 +1,133 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tharchen <tharchen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/16 21:35:21 by tharchen          #+#    #+#             */
+/*   Updated: 2021/07/13 12:12:54 by tharchen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <dispatcher.hpp>
+
+void 	print_elem(int & elem)
+{
+	std::cout << "[" << elem << "] ";
+}
+
+int		main(void)
+{
+	{
+		std::list<int> lst;
+
+		for (size_t i = 0; i < 20; i++)
+			lst.push_back(i);
+
+		try
+		{
+			for_each(lst.begin(), lst.end(), print_elem);
+			std::cout << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << "error: easyfind: " << e.what() << std::endl;
+		}
+		std::cout << C_G_RED"<--- --- --- --- --- --- --- --- --- --- --- --- ---"C_RES << std::endl;
+		try
+		{
+			for_each(easyfind(lst, -4), lst.end(), print_elem);
+			std::cout << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << "error: easyfind: " << e.what() << std::endl;
+		}
+		std::cout << C_G_RED"<--- --- --- --- --- --- --- --- --- --- --- --- ---"C_RES << std::endl;
+		try
+		{
+			for_each(easyfind(lst, 'A'), lst.end(), print_elem);
+			std::cout << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << "error: easyfind: " << e.what() << std::endl;
+		}
+		std::cout << C_G_RED"<--- --- --- --- --- --- --- --- --- --- --- --- ---"C_RES << std::endl;
+		try
+		{
+			for_each(easyfind(lst, 3), lst.end(), print_elem);
+			std::cout << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << "error: easyfind: " << e.what() << std::endl;
+		}
+		std::cout << C_G_RED"<--- --- --- --- --- --- --- --- --- --- --- --- ---"C_RES << std::endl;
+		try
+		{
+			for_each(easyfind(lst, 12), lst.end(), print_elem);
+			std::cout << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << "error: easyfind: " << e.what() << std::endl;
+		}
+		std::cout << C_G_RED"<--- --- --- --- --- --- --- --- --- --- --- --- ---"C_RES << std::endl;
+		try
+		{
+			for_each(easyfind(lst, 0), lst.end(), print_elem);
+			std::cout << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << "error: easyfind: " << e.what() << std::endl;
+		}
+		std::cout << C_G_RED"<--- --- --- --- --- --- --- --- --- --- --- --- ---"C_RES << std::endl;
+		try
+		{
+			for_each(easyfind(lst, 19), lst.end(), print_elem);
+			std::cout << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << "error: easyfind: " << e.what() << std::endl;
+		}
+		std::cout << C_G_RED"<--- --- --- --- --- --- --- --- --- --- --- --- ---"C_RES << std::endl;
+		try
+		{
+			for_each(easyfind(lst, 190), lst.end(), print_elem);
+			std::cout << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << "error: easyfind: " << e.what() << std::endl;
+		}
+		std::cout << C_G_RED"<--- --- --- --- --- --- --- --- --- --- --- --- ---"C_RES << std::endl;
+		try
+		{
+			for_each(easyfind(lst, uint(42)), lst.end(), print_elem);
+			std::cout << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << "error: easyfind: " << e.what() << std::endl;
+		}
+		std::cout << C_G_RED"<--- --- --- --- --- --- --- --- --- --- --- --- ---"C_RES << std::endl;
+		for (int i = -5; i < 25; i++)
+		{
+			try
+			{
+				for_each(easyfind(lst, int(i)), lst.end(), print_elem);
+				std::cout << std::endl;
+			}
+			catch (std::exception & e)
+			{
+				std::cout << "error: easyfind: " << e.what() << std::endl;
+			}
+		}
+		std::cout << C_G_RED"<--- --- --- --- --- --- --- --- --- --- --- --- ---"C_RES << std::endl;
+	}
+	return (0);
+}
